a113cddcd6fa147549e8be9419741ddef8f5a3c8
Mark Patrick <M.Patrick@Outlook.com>
2020-12-19 22:47:54 -0500

    day-04

---
+++ Day-04/solution.py
@@ -0,0 +1,46 @@
+import re
+import os
+
+dirname = os.path.abspath('')
+filename = os.path.join(dirname,'input.txt')
+with open(filename,"r") as f:
+    passports = f.read().split('\n\n')
+    passports = [passport.replace('\n',' ') for passport in passports]
+
+
+# Passport dictionary
+ppdict = {}
+i = 0
+for passport in passports:
+    ppmatch = re.findall('(\S+):(\S+)',passport)
+    ppdict[i] = dict(ppmatch)
+    i += 1
+# Part 1 Answer
+validpp = 0
+required_fields = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
+for pp in ppdict.values():
+    if all(req in pp for req in required_fields):
+        validpp+=1
+print(validpp)
+
+# Part 2 Answer
+validpps = {}
+i = 0
+required_fields = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
+for pp in ppdict.values():
+    if all(req in pp for req in required_fields):
+        if (
+            (len(pp['byr'])==4 and (1920<= int(pp['byr'])<=2002)) and
+            (len(pp['iyr'])==4 and (2010<= int(pp['iyr'])<=2020)) and
+            (len(pp['eyr'])==4 and (2020<= int(pp['eyr'])<=2030)) and
+            ((pp['hgt'][-2:] == 'cm' and 150 <= int(pp['hgt'][:-2]) <= 193) or
+             (pp['hgt'][-2:] == 'in' and 59 <= int(pp['hgt'][:-2]) <= 76)) and
+            (re.match(r'#[\da-f]{6}',pp['hcl'])) and
+            (pp['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']) and
+            (len(pp['pid'])==9 and re.match(r'\d{9}',pp['pid']))
+            
+           ):
+            validpps[i]=pp
+            i+=1
+print(len(validpps))
+    
